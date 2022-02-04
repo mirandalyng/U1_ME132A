90a1880fed83bfc4fdb51949105e4efff5e10c86
mirandalyng <lyngmiranda@gmail.com>
2022-02-04 15:17:57 +0100

    Finished 4e
    Added the nested loops :
    
    //NESTED LOOPS
    //          for (let c = 0; c < C; c++){
    
    //                 for (let r = 0; r < R; r++){
    //                   gridContainer.appendChild(createNumberDiv());
    //                 }
    //         }
    
    And the single loop
       //ONE LOOP
            let rowCol = R*C;
            for(let i = 0; i < rowCol; i++){
              gridContainer.appendChild(createNumberDiv());
            }
        }

--- U1 (01.02.2022)/Averager/4e) Fill the grid - create RxC numberDivs/index.js
+++ U1 (01.02.2022)/Averager/4e) Fill the grid - create RxC numberDivs/index.js
@@ -14,5 +14,40 @@ VIDEO:  Record a video where you explain the two different ways (see above) of c
         amount of numberDivs. The video should be called loopExplainer
 
 */
+function createNumberDiv (){
+         let addDiv = document.createElement("div");
+         addDiv.innerHTML = randomNumber(100);
+    
+         return addDiv;
+}
+    
 
+function gridMaker (gridContainer, R, C){
+         gridContainer.style.display = "grid";
+         gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)`;
+         gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)`;
+
+//         //NESTED LOOPS 
+//          for (let c = 0; c < C; c++){
+
+//                 for (let r = 0; r < R; r++){     
+//                   gridContainer.appendChild(createNumberDiv());
+//                 }
+//         }
+// 
+         
+
+        
+
+        //ONE LOOP
+        let rowCol = R*C;
+        for(let i = 0; i < rowCol; i++){
+          gridContainer.appendChild(createNumberDiv());
+        }
+    }
+   
+    
+function randomNumber ( max ) {
+        return Math.floor( max * Math.random() );
+}
 
